f2962f835d9b2f163a80a72e8b1ea2ade17ef151
danielto1404 <daniilkorolev00@gmail.com>
2023-05-16 12:02:41 +0300

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # aleo-recommender
+
 Privacy preserving recommender systems using CatBoost and Aleo
+
+## Usage:
+
+1. Train the model
+
+```python
+
+```
+
+2. Transpile the model to Aleo
+
+```shell
+python leo-transpiler/transpile.py --model_path=model.pkl --model-type=xgboost --output=leo-contrants
+```
--- leo-transpiler/transpile.py
+++ leo-transpiler/transpile.py
@@ -2,20 +2,19 @@ import argparse
 import os
 
 
-def transpile(args: argparse.Namespace):
+def transpile(model: str, model_type: str, output: str):
     pass
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, required=True,
-                        help="Path to model")
-    parser.add_argument("--model-type", type=str, required=True, default="xgboost",
-                        help="Type of model, supports: [xgboost]")
+                        help="Path to model file")
+    parser.add_argument("--model-type", type=str, required=True, default="xgboost", choices=["xgboost"],
+                        help="Model type to transpile")
     parser.add_argument("--output", type=str, required=True,
-                        help="Path to output folder for Aleo Smart Contract")
+                        help="Path to output folder for Aleo Smart-Contracts")
 
     _args = parser.parse_args()
 
     os.makedirs(_args.output, exist_ok=True)
-
